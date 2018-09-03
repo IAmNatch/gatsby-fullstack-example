5d2c5b2532c965d62610d05ff6ed559137248f4d
Shane Robbins <shanerobbins@A-Wild-Macbook-Appeared.local>
2018-09-03 18:10:12 -0400

    font loading optimizations

--- site/src/layouts/index.js
+++ site/src/layouts/index.js
@@ -4,7 +4,6 @@ import Helmet from 'react-helmet';
 
 import './index.css';
 import 'normalize.css';
-import 'roboto-fontface';
 
 const Layout = ({ children, data }) => (
   <div>
@@ -37,6 +36,14 @@ const Layout = ({ children, data }) => (
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
+      <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js"></script>
+<script>
+ WebFont.load({
+    google: {
+      families: ['Roboto']
+    }
+  });
+</script>
     </Helmet>
     <div>{children()}</div>
   </div>
